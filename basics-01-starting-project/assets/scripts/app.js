4a53fb8f86fd7d72cd61a0189beb3a374c2d7480
aniquaTabassum <aniqua.tabassum337@gmail.com>
2020-04-12 21:27:30 +0600

    added a custom function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 currentResult = currentResult + 10;
-currentDescription = defaultResult+' + 10';
+currentDescription = defaultResult + " + 10";
 outputResult(currentResult, currentDescription);
+add(1, 2);
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("the result is " + result);
+}
