3fbec6ccf92b13da8913f6a185375c9b96567a94
aniquaTabassum <aniqua.tabassum337@gmail.com>
2020-04-12 21:11:44 +0600

    First assignment done

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult = currentResult + 10;
+currentDescription = defaultResult+' + 10';
+outputResult(currentResult, currentDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,8 @@
+let userInput = 377;
+let result = 0;
+result = result + userInput;
+result -= 0;
+result *= userInput;
+result /= userInput;
+alert(userInput);
+alert(result);
